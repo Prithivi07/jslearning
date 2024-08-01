75e33b2e60a3485d8c461c832ec8129385ba81ea
Prithivi07 <prithivirajan2406@gmail.com>
2024-08-01 02:44:22 -0400

    employeeManagement2

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,8 +1,8 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-    { id: 4, name: 'Prithivi Rajan', age: 22, department: 'R & D', salary: 10000 }
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 ,special :'Java'},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, special: 'C' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, special: 'Javascript' },
+    { id: 4, name: 'Prithivi Rajan', age: 22, department: 'R & D', salary: 10000, spacial: 'Python' }
   ];
 
   function displayEmployees() {
@@ -28,6 +28,16 @@ function displayHREmployees() {
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if (foundEmployee) {
     document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+
+    }
+  }
+  function findEmployeeBySpecial(empSpecial) {
+    const output = employees.find(employee => employee.special === 'Javascript')
+     if (output) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${output.id}: ${output.name}: ${output.name} - ${output.department} - $${output.salary}</p>`;
     }
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
